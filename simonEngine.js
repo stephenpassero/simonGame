7cb1dc740138d63ff6069ea609065e51912989eb
Stephen Passero <stephen.passero@gmail.com>
2017-08-21 09:57:41 -0400

    Add a pause between buttons flashing and deactivate the start button unless the game is over

--- simonEngine.js
+++ simonEngine.js
@@ -8,25 +8,27 @@ $(document).ready(function() {
         if (inactivityTimer) {
             clearTimeout(inactivityTimer);
         }
-
         inactivityTimer = setTimeout(() => {
             listeningMode = false;
             console.log('You lose');
+            running = false;
         }, 4000);
     }
 
     function onButtonPressed() {
-        if(!buttonsClicked[buttonsClicked.length - 1] === numbers[buttonsClicked.length - 1]){
-            console.log('You lose');
+        if (buttonsClicked[buttonsClicked.length - 1] !== numbers[buttonsClicked.length - 1]) {
             listeningMode = false;
+            console.log('You lose');
+            running = false;
         }
         if (buttonsClicked.length === numbers.length) {
             listeningMode = false;
-            startInactivityTimer();
+            clearTimeout(inactivityTimer);
             start();
+        } else {
+            startInactivityTimer();
         }
     }
-
     $('#button1').click(() => {
         if (listeningMode === true) {
             buttonsClicked.push(1);
@@ -58,70 +60,68 @@ $(document).ready(function() {
             $('#strict').addClass('active');
         }
     });
-
-    function playSequence(numbers) {
-        for (let index = 0; index < numbers.length; index++) {
-            const nextNumber = numbers[index];
-            if (nextNumber === 1) {
-                $('#button' + nextNumber).css('background-color', '#13ff7c');
-                setTimeout(() => {
-                    $('#button' + nextNumber).css('background-color', '#090');
-                }, 400);
-            } else if (nextNumber === 2) {
-                $('#button' + nextNumber).css('background-color', '#ff4c4c');
-                setTimeout(() => {
-                    $('#button' + nextNumber).css('background-color', '#bc2000');
-                }, 400);
-            } else if (nextNumber === 3) {
-                $('#button' + nextNumber).css('background-color', '#fed93f');
-                setTimeout(() => {
-                    $('#button' + nextNumber).css('background-color', '#bcbc20');
-                }, 400);
-            } else if (nextNumber === 4) {
-                $('#button' + nextNumber).css('background-color', '#1c8cff');
-                setTimeout(() => {
-                    $('#button' + nextNumber).css('background-color', '#0002bc');
-                }, 400);
-            }
+    let index = 0;
+    function delayedLoop() {
+        const nextNumber = numbers[index];
+        if (nextNumber === 1) {
+            $('#button' + nextNumber).css('background-color', '#13ff7c');
+            setTimeout(() => {
+                $('#button' + nextNumber).css('background-color', '#090');
+            }, 400);
+        } else if (nextNumber === 2) {
+            $('#button' + nextNumber).css('background-color', '#ff4c4c');
+            setTimeout(() => {
+                $('#button' + nextNumber).css('background-color', '#bc2000');
+            }, 400);
+        } else if (nextNumber === 3) {
+            $('#button' + nextNumber).css('background-color', '#fed93f');
+            setTimeout(() => {
+                $('#button' + nextNumber).css('background-color', '#bcbc20');
+            }, 400);
+        } else if (nextNumber === 4) {
+            $('#button' + nextNumber).css('background-color', '#1c8cff');
+            setTimeout(() => {
+                $('#button' + nextNumber).css('background-color', '#0002bc');
+            }, 400);
         }
+
+        if(++index === numbers.length) {
+            return;
+        }
+
+        window.setTimeout(delayedLoop, 1000);
+    }
+
+
+    function playSequence() {
+        index = 0;
+        delayedLoop();
     }
 
     function addAnotherRandomNumber(numbers) {
         numbers.push(Math.floor((Math.random() * 4) + 1));
     }
 
-    function getUserResponse() {
-        if (listeningMode) {
-            setTimeout(getUserResponse, 0);
-            return [0, 1];
-        } else {
-            return buttonsClicked;
-        }
-    }
-
-    function arraysEqual(a, b) {
-        for (var i = 0; i < a.length; ++i) {
-            if (a[i] !== b[i]) return false;
-        }
-        return true;
-    }
-
     function updateRound(round) {
         $('#count').html(round);
     }
     let numOfRounds = 0;
-    function start(){
-        if(numOfRounds < 20){
+    let running = false;
+
+    function start() {
+        running = true;
+        if (numOfRounds < 20) {
             numOfRounds++;
             updateRound(numOfRounds);
             addAnotherRandomNumber(numbers);
-            playSequence(numbers);
+            playSequence();
             listeningMode = true;
             startInactivityTimer();
         }
     }
-
     $('#start').click(() => {
-        start();
+        if (!running) {
+            start();
+        }
     });
 });
